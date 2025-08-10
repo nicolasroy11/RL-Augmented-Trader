739b6f13c6fb64efacb519a438fa480e0cf54410
Nicolas Roy <nicolasroy11@gmail.com>
2025-08-11 02:53:22 +0400

    updated urls

--- services/data_app/urls.py
+++ services/data_app/urls.py
@@ -1,8 +1,56 @@
-from services.service_app.views import current_datetime
+from rest_framework.schemas import get_schema_view
+import inspect
+from django.urls import path, re_path
+from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+from services.data_app.views.data_view import Ticks
 
-from django.urls import path
-from services.service_app.views import current_datetime
 
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=True):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["https", "http"]
+        return schema
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ticks API",
+        default_version='v1',
+        description="<b>Endpoints for the tick data API</b>",
+    ),
+    public=True,
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
-    path('current_datetime', current_datetime, name='current_datetime'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# generate urlpatterns from each method in the view class
+grids_method_list = inspect.getmembers(Ticks, predicate=inspect.isfunction)
+for i, method in grids_method_list:
+
+    view = method.view
+    if method.include_in_swagger:
+        api_decorated_view = api_view([method.http_method])(method.view)
+        swagger_view = swagger_auto_schema(
+            method=method.http_method,
+            responses={200: method.return_type},
+            query_serializer=method.query_params_type,
+            operation_id=method.summary,
+            operation_description=method.description,
+            tags=[method.group]
+        )(api_decorated_view)
+        view = swagger_view
+
+    # we ignore the methods that do not have the http_method, which is
+    # only present for those decorated with the custom @Http decorator
+    urlpatterns.append(path(Ticks.url + method.path, view))
--- services/urls.py
+++ services/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from service_app import urls
+from data_app import urls as data_app_urls
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("api/", include(urls)),
+    path("api/data/", include(data_app_urls))
 ]

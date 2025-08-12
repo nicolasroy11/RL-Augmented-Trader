8c61262fa5215539421b11ecc4401968a3bcce94
Nicolas Roy <nicolasroy11@gmail.com>
2025-08-12 13:28:50 +0400

    Update urls.py

--- services/urls.py
+++ services/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/data/", include(data_app_urls))
+    path("api/", include(data_app_urls))
 ]

38ec558008f8f5c4023249c95651d727ff81c896
Nicolas Roy <nicolasroy11@gmail.com>
2025-08-14 01:35:49 +0400

    Update visualization.py

--- db/visualization.py
+++ db/visualization.py
@@ -1,40 +1,52 @@
 import finplot as fplt
+import numpy as np
 import pandas as pd
-
+from db._helpers import get_all_duckdb_archive_files
 from db.data_store import DataStore
-from db.settings import DB_FILE_PATH
 
 
-# Fetch the data
-df = DataStore(DB_FILE_PATH, readonly=True).get_all_tick_data()
-df['timestamp'] = pd.to_datetime(df['timestamp'])
-df.set_index('timestamp', inplace=True)
 
-# Create a new window
-price_ax, rsi_ax, macd_ax = fplt.create_plot('BTCUSDC Indicators', rows=3)
+files = get_all_duckdb_archive_files()
 
-# Plot price
-fplt.plot(df['price'], ax=price_ax, legend='Price')
+for path in files:
+    data_store = DataStore(db_path=path, readonly=True)
 
-# Plot EMAs
-fplt.plot(df['ema_short'], ax=price_ax, color='#5f5', legend='EMA Short')
-fplt.plot(df['ema_mid'], ax=price_ax, color='#55f', legend='EMA Mid')
-fplt.plot(df['ema_long'], ax=price_ax, color='#f55', legend='EMA Long')
-fplt.plot(df['ema_xlong'], ax=price_ax, color='#aaa', legend='EMA XLong')
+    # Fetch the data
+    df = data_store.get_all_tick_data()
+    df['timestamp'] = pd.to_datetime(df['timestamp']).astype(np.int64) * 10**3
+    df.set_index('timestamp', inplace=True)
+    df.index = pd.to_datetime(df.index)
 
-# Plot RSI indicators
-fplt.plot(df['rsi_5'], ax=rsi_ax, legend='RSI 5')
-fplt.plot(df['rsi_7'], ax=rsi_ax, legend='RSI 7')
-fplt.plot(df['rsi_12'], ax=rsi_ax, legend='RSI 12')
-# fplt.add_line((df.index[0], 70), (df.index[-1], 70), ax=rsi_ax, color='#f55', style='--')  # Overbought
-# fplt.add_line((df.index[0], 30), (df.index[-1], 30), ax=rsi_ax, color='#5f5', style='--')  # Oversold
+    # Create a new window
+    price_ax, rsi_ax, macd_ax = fplt.create_plot('BTCUSDC Indicators', rows=3)
 
-# Plot MACD
-fplt.plot(df['macd_line'], ax=macd_ax, color='cyan', legend='MACD Line')
-fplt.plot(df['macd_signal'], ax=macd_ax, color='orange', legend='MACD Signal')
-colors = df['macd_hist'].apply(lambda x: '#0f0' if x >= 0 else '#f00').tolist()
-fplt.plot(df['macd_hist'], ax=macd_ax, colors=colors, legend='MACD Histogram')
+    # Plot price
+    fplt.plot(df['price'], ax=price_ax, legend='Price')
+
+    # Plot EMAs on price ax
+    fplt.plot(df['ema_short'], ax=price_ax, color='#5f5', legend='EMA Short')
+    fplt.plot(df['ema_mid'], ax=price_ax, color='#55f', legend='EMA Mid')
+    fplt.plot(df['ema_long'], ax=price_ax, color='#f55', legend='EMA Long')
+    fplt.plot(df['ema_xlong'], ax=price_ax, color='#aaa', legend='EMA XLong')
+
+    # Plot Bollinger bands on price ax
+    fplt.plot(df['bb_upper'], ax=price_ax, color='#000')
+    fplt.plot(df['bb_middle'], ax=price_ax, color='#333')
+    fplt.plot(df['bb_lower'], ax=price_ax, color='#000')
+
+    # Plot RSI indicators
+    fplt.plot(df['rsi_5'], ax=rsi_ax, legend='RSI 5')
+    fplt.plot(df['rsi_7'], ax=rsi_ax, legend='RSI 7')
+    fplt.plot(df['rsi_12'], ax=rsi_ax, legend='RSI 12')
+    # fplt.add_line((df.index[0], 70), (df.index[-1], 70), ax=rsi_ax, color='#f55', style='--')  # Overbought
+    # fplt.add_line((df.index[0], 30), (df.index[-1], 30), ax=rsi_ax, color='#5f5', style='--')  # Oversold
+
+    # Plot MACD
+    fplt.plot(df['macd_line'], ax=macd_ax, color='cyan', legend='MACD Line')
+    fplt.plot(df['macd_signal'], ax=macd_ax, color='orange', legend='MACD Signal')
+    colors = df['macd_hist'].apply(lambda x: '#0f0' if x >= 0 else '#f00').tolist()
+    fplt.plot(df['macd_hist'], ax=macd_ax, colors=colors, legend='MACD Histogram')
 
 
-# Auto-align and run the UI
-fplt.show()
+    # Auto-align and run the UI
+    fplt.show()

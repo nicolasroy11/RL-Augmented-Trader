a011e4897304c7166b3df76e507ac8092ea12b88
Nicolas Roy <nicolasroy11@gmail.com>
2025-08-14 01:35:39 +0400

    Update settings.py

--- db/settings.py
+++ db/settings.py
@@ -2,5 +2,6 @@ import os
 
 
 DB_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-DB_FILE_PATH = os.path.join(DB_DIRECTORY, "ticks.duckdb")
+DUCKDB_ARCHIVES_PATH = os.path.join(DB_DIRECTORY, "archives")
+DUCKDB_FILE_PATH = os.path.join(DB_DIRECTORY, "ticks.duckdb")
 SYMBOL = 'BTCFDUSD'

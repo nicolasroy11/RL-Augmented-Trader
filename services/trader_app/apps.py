93095adf6a3095a4b92847827a8e976e5bf02f77
Nicolas Roy <nicolasroy11@gmail.com>
2025-08-20 21:50:36 +0400

    Update apps.py

--- services/trader_app/apps.py
+++ services/trader_app/apps.py
@@ -1,18 +1,5 @@
-from dataclasses import dataclass
-from datetime import datetime, timezone
-from typing import List
 from django.apps import AppConfig
-import time
-import pandas as pd
-
-from classes import Balances, OrderReport
-from helpers import do_limit_buy, do_limit_sell, get_balances_snapshot, get_instant_notional_minimum
 import runtime_settings
-import numpy as np
-import torch
-from RL.playground.stochastic.actor_critic import ActorCritic
-from services.types import Actions
-
 
 class TradersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -28,4 +15,4 @@ class TradersConfig(AppConfig):
             price='2000.00'
         )
         if client_test != {}:
-            t = 0
+            raise RuntimeError()

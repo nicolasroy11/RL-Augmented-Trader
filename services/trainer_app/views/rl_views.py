37502c1527c72ca6b7eac206d78da24f0fbfc862
Nicolas Roy <nicolasroy11@gmail.com>
2025-09-16 15:40:54 -0700

    added run_xgboost_futures to API choices

--- services/trainer_app/views/rl_views.py
+++ services/trainer_app/views/rl_views.py
@@ -1,7 +1,9 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 import runtime_settings
-from services.core.dtos.policy_gradient_results_dto import PolicyGradientResultsDto
+from services.core.ML.configurations.XGBoost.xgboost_strategy import XGBMultiClassModel, run_futures_xgb_pipeline, train_test_walk_forward_on_futures
+from services.core.ML.configurations.fixture_config import PPO_TCN_FUTURES_SET_ID
+from services.core.dtos.policy_gradient_results_dto import PPOTCNTrainingResults
 from services.decorators.decorators.view_decorator import View
 from services.decorators.decorators.view_class_decorator import ViewClass
 from services.core.ML.configurations.PPO_flattened_history.train import RLRepository
@@ -21,7 +23,7 @@ class RLViews:
     @View(
         path='run_policy_gradient',
         http_method='GET',
-        return_type=PolicyGradientResultsDto.Serializer(),
+        return_type=PPOTCNTrainingResults.Serializer(),
         description='Run policy gradient algorithm and return results',
         include_in_swagger=True
     )
@@ -29,7 +31,7 @@ class RLViews:
         def exec():
             rl_repo = RLRepository()
             results = rl_repo.run_policy_gradient(window_size=runtime_settings.DATA_TICKS_WINDOW_LENGTH, num_episodes=100)
-            dto = PolicyGradientResultsDto.Serializer(results).data
+            dto = PPOTCNTrainingResults.Serializer(results).data
             return JsonResponse(dto)
         return exec()
 
@@ -37,7 +39,7 @@ class RLViews:
     @View(
         path='run_ppo',
         http_method='POST',
-        return_type=PolicyGradientResultsDto.Serializer(),
+        return_type=PPOTCNTrainingResults.Serializer(),
         description='Run policy gradient algorithm and return results',
         include_in_swagger=True
     )
@@ -47,7 +49,7 @@ class RLViews:
         def exec():
             rl_repo = RLRepository()
             results = rl_repo.run_ppo(window_size=runtime_settings.DATA_TICKS_WINDOW_LENGTH, num_episodes=100)
-            dto = PolicyGradientResultsDto.Serializer(results).data
+            dto = PPOTCNTrainingResults.Serializer(results).data
             return JsonResponse(dto)
         return exec()
     
@@ -55,7 +57,7 @@ class RLViews:
     @View(
         path='run_ppo_futures',
         http_method='POST',
-        return_type=PolicyGradientResultsDto.Serializer(),
+        return_type=PPOTCNTrainingResults.Serializer(),
         description='Run policy gradient algorithm and return results',
         include_in_swagger=True
     )
@@ -65,7 +67,7 @@ class RLViews:
         def exec():
             rl_repo = RLRepository()
             results = rl_repo.run_ppo(window_size=runtime_settings.DATA_TICKS_WINDOW_LENGTH, num_episodes=100)
-            dto = PolicyGradientResultsDto.Serializer(results).data
+            dto = PPOTCNTrainingResults.Serializer(results).data
             return JsonResponse(dto)
         return exec()
     
@@ -73,7 +75,7 @@ class RLViews:
     @View(
         path='run_ppo_tcn_futures',
         http_method='POST',
-        return_type=PolicyGradientResultsDto.Serializer(),
+        return_type=PPOTCNTrainingResults.Serializer(),
         description='Run policy gradient algorithm and return results',
         include_in_swagger=True
     )
@@ -82,8 +84,36 @@ class RLViews:
         # num_episodes=100, gamma=0.99, lr=1e-4, clip_epsilon=0.2, ppo_epochs=4, batch_size=64
         def exec():
             rl_repo = PPO_TCN_Repo()
-            results = rl_repo.run_ppo(window_size=runtime_settings.DATA_TICKS_WINDOW_LENGTH, num_episodes=100, is_futures=True)
-            dto = PolicyGradientResultsDto.Serializer(results).data
+            results = rl_repo.run_ppo(window_size=runtime_settings.DATA_TICKS_WINDOW_LENGTH, feature_set_id = PPO_TCN_FUTURES_SET_ID,num_episodes=100, is_futures=True)
+            dto = PPOTCNTrainingResults.Serializer(results).data
             return JsonResponse(dto)
         return exec()
     
+
+    @View(
+        path='run_xgboost_futures',
+        http_method='POST',
+        return_type=PPOTCNTrainingResults.Serializer(),
+        description='Run policy gradient algorithm and return results',
+        include_in_swagger=True
+    )
+    def run_xgboost_futures(req: WSGIRequest):
+        feature_set = req.GET.get('feature_set')
+        def exec():
+
+            result = run_futures_xgb_pipeline(
+                horizon_ticks=6,
+                fee_bps=2.0,
+                min_edge_bps=1.0,
+                xgb_params=None,          # or dict of overrides
+                n_splits=5,
+                min_confidence=0.40,
+                fee_bps_roundtrip=4.0,
+                artifact_name="futures_walkforward.json",
+                # env=my_env,               # optional: run backtest on your env
+                # env_results_path="/tmp/env_bt.csv",  # optional
+            )
+            print(result)
+
+            # return JsonResponse(dto)
+        return exec()
